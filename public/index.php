60b56522ab1f2450291129b7c11f1e1ebed65a03
Kirill <gluck1986@gmail.com>
2018-10-03 17:48:37 +0400

    di 3

--- public/index.php
+++ public/index.php
@@ -7,37 +7,62 @@
  */
 declare(strict_types=1);
 
-use Psr\Http\Message\ServerRequestInterface;
+use App\Http\Resolver\Resolver;
+use App\Http\Router\AuraRouterAdapter;
+use App\Http\Router\RouterInterface;
+use Aura\Router\RouterContainer;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Zend\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
 use Zend\ServiceManager\ServiceManager;
-
-
 
 require '../vendor/autoload.php';
 
 $serverRequest = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
-
+//################### Инициализация, настройка инверсии
 $di = new ServiceManager([
     'abstract_factories'=>[ReflectionBasedAbstractFactory::class],
-    'factories' => [],
+    'factories' => [
+        RouterInterface::class=> function (ContainerInterface $container) {
+            return new AuraRouterAdapter($container->get(RouterContainer::class));
+        },
+        Resolver::class => function (ContainerInterface $container) {
+            return new App\Http\Resolver\Resolver($container);
+        }
+    ],
     'services' => []
 ]);
 
+//################### лямбда для сокращения кода
+$r = function ($handle) use ($di): MiddlewareInterface {
+    /** @var Resolver $resolver */
+    $resolver = $di->get(Resolver::class);
+
+    return $resolver->resolve($handle);
+};
+//###################  создание и настройка конвеера
 
 $pipeline = new \Zend\Stratigility\MiddlewarePipe();
-$router = new \App\Http\Router\AuraRouterAdapter(new \Aura\Router\RouterContainer());
-$resolver = new \App\Http\Resolver\Resolver($di);
 
-/** todo routes */
 
+//################### настройка маршрутизатора
+$router = $di->get(RouterInterface::class);
+$router->add(['GET'], 'home','/', $r(function () {
+    return new \Zend\Diactoros\Response\TextResponse('i am a simple home page');
+}));
+
+//################### получение текущего маршрута
 $serverRequest = $router->match($serverRequest);
 
+//################### получение обработчика маршрута
 $handler = $serverRequest->getAttribute('handler');
 
-$pipeline->pipe($resolver->resolve($handler));
+//################### диспетчерезация
+$pipeline->pipe($r($handler));
 
+//################### запуск конвеера
 $response = $pipeline->handle($serverRequest);
 
+//################### отправка результата в браузер
 $emitter = new \Narrowspark\HttpEmitter\SapiEmitter();
-
 $emitter->emit($response);
